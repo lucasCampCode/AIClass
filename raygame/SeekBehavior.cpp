78177952e80b38f938f0c3556712fd9d320a8555
lucas <lcampbell0815@gmail.com>
2021-03-01 17:10:23 -0600

    did more on seek behavior

--- raygame/SeekBehavior.cpp
+++ raygame/SeekBehavior.cpp
@@ -1,1 +1,10 @@
 #include "SeekBehavior.h"
+#include "Agent.h"
+MathLibrary::Vector2 SeekBehavior::calculateForce(Agent* agent)
+{
+	MathLibrary::Vector2 direction = MathLibrary::Vector2::normalize(m_target->getWorldPosition() - agent->getWorldPosition());
+	
+	
+
+	return MathLibrary::Vector2();
+}
